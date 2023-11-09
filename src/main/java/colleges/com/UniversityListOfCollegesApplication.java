01d7e6ae8b839a7527de9b0ec2dcebe2f93c2b71
MD NABI ALAM <nabialamspring.com>
2023-11-09 23:17:28 +0530

    Create Project Only

---
+++ src/main/java/colleges/com/UniversityListOfCollegesApplication.java
@@ -0,0 +1,13 @@
+package colleges.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityListOfCollegesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityListOfCollegesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/colleges/com/UniversityListOfCollegesApplicationTests.java
@@ -0,0 +1,13 @@
+package colleges.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityListOfCollegesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
